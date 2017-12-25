26436209e9612782cfaa72d5698e9c8409b35c75
Eric Lubisse <ericl@digiterre.com>
2017-12-25 18:51:14 +0000

    Forcing docker compose to rebuild images

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ git clone https://github.com/chosenbreed37/quotes-app.git
 cd quotes-app
 sudo docker-compose down
 mv ./ui/src/config/config.prod.js ./ui/src/config/config.js
-sudo docker-compose up -d
+sudo docker-compose up --build -d
 sudo docker exec -it quotesapp_db_1 mongoimport --verbose --db quotes --collection quotes --type tsv --headerline --file /tmp/data/quotes.tsv --drop
 echo "Quotes App Node running on  on port 3000..."

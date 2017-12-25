6f56582c983e76470b628a2ea95fce11b96b8775
Eric Lubisse <ericl@digiterre.com>
2017-12-25 18:40:17 +0000

    Revising configuration options for ui

--- ui/src/config/config.dev.js
+++
@@ -1,7 +0,0 @@
-const config = {};
-
-config.api_url = process.env.API_URL || 'http://192.168.99.100:3003';
-config.env = process.env.NODE_ENV || 'development';
-
-export default config;
-
---
+++ ui/src/config/config.js
@@ -0,0 +1,6 @@
+const config = {};
+
+config.env = "http://192.168.99.100:3003";
+
+export default config;
+

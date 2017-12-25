ddfa19a64bb4d775ace83e91c1ce460fdd59b235
Eric Lubisse <ericl@digiterre.com>
2017-12-25 18:47:00 +0000

    Fixing path error in deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ rm -rf quotes-app
 git clone https://github.com/chosenbreed37/quotes-app.git
 cd quotes-app
 sudo docker-compose down
-mv ./ui/config/config.prod.js ./ui/config/config.js
+mv ./ui/src/config/config.prod.js ./ui/src/config/config.js
 sudo docker-compose up -d
 sudo docker exec -it quotesapp_db_1 mongoimport --verbose --db quotes --collection quotes --type tsv --headerline --file /tmp/data/quotes.tsv --drop
 echo "Quotes App Node running on  on port 3000..."

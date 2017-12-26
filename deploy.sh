f3c54be6f553ac8f4cd5645b58659c50e3901ee1
Eric Lubisse <ericl@digiterre.com>
2017-12-26 17:20:31 +0000

    A few adjustment to deployment script

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,8 @@ rm -rf quotes-app
 git clone https://github.com/chosenbreed37/quotes-app.git
 cd quotes-app
 sudo docker-compose down
+sudo docker rmi quotesapp_ui
+sudo docker rmi quotesapp_api
 mv ./ui/src/config/config.prod.js ./ui/src/config/config.js
 sudo docker-compose up --build -d
 sudo docker exec -it quotesapp_db_1 mongoimport --verbose --db quotes --collection quotes --type tsv --headerline --file /tmp/data/quotes.tsv --drop

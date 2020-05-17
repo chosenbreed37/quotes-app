93a73548163aca2d13e7219f88d24aca4cac5106
chosenbreed37 <ericl@digiterre.com>
2020-05-17 16:23:41 +0100

    Fixing build for live environment

--- ui/src/config/config.js
+++ ui/src/config/config.js
@@ -1,6 +1,11 @@
 const config = {};
 
-config.api_url = "http://localhost:3003";
+const host = (process.env.REACT_APP_API_HOST || 'localhost').trim();
+const port = (process.env.REACT_APP_API_PORT || '3003').trim();
+
+config.api_url = `http://${host}:${port}`;
+
+console.log('>>> config: ', config);
 
 export default config;
 
--- ui/src/config/config.prod.js
+++
@@ -1,6 +0,0 @@
-const config = {};
-
-config.api_url = "http://138.68.176.15:3003";
-
-export default config;
-
